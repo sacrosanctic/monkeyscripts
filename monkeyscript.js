e25f99bda2c9634f84c751fd44f23911ac3bef17
Scott Wu <sw@scottwu.ca>
2026-01-03 10:54:06 +0800

    Change button text to 'Submit' and update version to 2026-01-03-1054

--- monkeyscript.js
+++ monkeyscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Monkey Script for Payment
 // @namespace    http://tampermonkey.net/
-// @version      2026-01-03-1044
+// @version      2026-01-03-1054
 // @description  try to take over the world!
 // @author       You
 // @match        https://payment.xinchuan.tw/request-payment/
@@ -25,7 +25,7 @@
             <form action="https://payment.xinchuan.tw/request-payment" style="margin: 20px; padding: 10px; border: 1px solid #ccc;">
                 <label for="customInput">Custom Input: </label>
 			    <input id="customInput" name="productId" />
-			    <button id="submitBtn" type="submit">go</button>
+			    <button id="submitBtn" type="submit">Submit</button>
 		    </form>
         `;
 

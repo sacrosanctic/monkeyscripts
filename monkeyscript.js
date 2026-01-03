8bdf8ac1fb8caf19a2708819fcbbd6a3b2962a78
Scott Wu <sw@scottwu.ca>
2026-01-03 11:05:51 +0800

    Update selector to .ant-spin-container > :first-child > :first-child and form action to /request-payment, version 2026-01-03-1105

--- monkeyscript.js
+++ monkeyscript.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Monkey Script for Payment
 // @namespace    http://tampermonkey.net/
-// @version      2026-01-03-1054
+// @version      2026-01-03-1105
 // @description  try to take over the world!
 // @author       You
-// @match        https://payment.xinchuan.tw/request-payment/
+// @match        https://payment.xinchuan.tw/request-payment
 // @match        http://localhost:5173/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=undefined.localhost
 // @updateURL    https://raw.githubusercontent.com/sacrosanctic/monkeyscripts/main/monkeyscript.js
@@ -17,12 +17,12 @@
 
     // 1. Identify the element you want to follow
     // Change '.target-selector' to the class, ID, or tag of the existing element
-    const targetElement = document.querySelector('body > :last-child');
+    const targetElement = document.querySelector('.ant-spin-container > :first-child > :first-child');
 
     if (targetElement) {
         // 2. Define your form HTML
         const formHTML = `
-            <form action="https://payment.xinchuan.tw/request-payment" style="margin: 20px; padding: 10px; border: 1px solid #ccc;">
+            <form action="/request-payment" style="margin: 20px; padding: 10px; border: 1px solid #ccc;">
                 <label for="customInput">Custom Input: </label>
 			    <input id="customInput" name="productId" />
 			    <button id="submitBtn" type="submit">Submit</button>

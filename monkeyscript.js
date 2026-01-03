507f87fbb7ffc27fbca649589122b2c4d4c9f9db
Scott Wu <sw@scottwu.ca>
2026-01-03 11:24:45 +0800

    Add MutationObserver to wait for dynamic element, update version to 2026-01-03-1124

--- monkeyscript.js
+++ monkeyscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Monkey Script for Payment
 // @namespace    http://tampermonkey.net/
-// @version      2026-01-03-1121
+// @version      2026-01-03-1124
 // @description  try to take over the world!
 // @author       You
 // @match        https://payment.xinchuan.tw/request-payment
@@ -15,19 +15,35 @@
 (function() {
     'use strict';
 
-    const targetElement = document.querySelector('.ant-spin-container > :first-child');
-    if (!targetElement) {
-        console.log('Target element .ant-spin-container > :first-child not found');
-        return;
+    function addForm() {
+        const targetElement = document.querySelector('.ant-spin-container > :first-child');
+        if (!targetElement) {
+            console.log('Target element .ant-spin-container > :first-child not found');
+            return;
+        }
+
+        const formHTML = `
+            <form style="margin: 20px; padding: 10px; border: 1px solid #ccc;">
+                <label for="customInput">Custom Input: </label>
+                <input id="customInput" name="productId" />
+                <button type="submit">Submit</button>
+            </form>
+        `;
+
+        targetElement.insertAdjacentHTML('afterbegin', formHTML);
     }
 
-    const formHTML = `
-        <form style="margin: 20px; padding: 10px; border: 1px solid #ccc;">
-            <label for="customInput">Custom Input: </label>
-            <input id="customInput" name="productId" />
-            <button type="submit">Submit</button>
-        </form>
-    `;
+    // Check immediately
+    addForm();
 
-    targetElement.insertAdjacentHTML('afterbegin', formHTML);
+    // If not found, wait for it with MutationObserver
+    if (!document.querySelector('.ant-spin-container > :first-child')) {
+        const observer = new MutationObserver(() => {
+            if (document.querySelector('.ant-spin-container > :first-child')) {
+                observer.disconnect();
+                addForm();
+            }
+        });
+        observer.observe(document.body, { childList: true, subtree: true });
+    }
 })();
